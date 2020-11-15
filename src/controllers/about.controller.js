a696a9287e77344e3f49e0fa62d4e96077c7a148
Augusto <lagrange.tesla@gmail.com>
2020-11-14 18:55:34 -0600

    Se corrigió la vista home y se corrigieron algunos detalles de la vista about

--- src/controllers/about.controller.js
+++ src/controllers/about.controller.js
@@ -1,8 +1,9 @@
 import views from '../views/about.html'
 import '../css/componentes.css'
+import '../css/style.css'
 
 export default () => {
-    
+
     const divElement = document.createElement('div');
     divElement.innerHTML = views;
     return divElement;
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,2 +1,4 @@
 import '../css/componentes.css';
 
+
+
