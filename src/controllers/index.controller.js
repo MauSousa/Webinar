f76f2f2de82b11782d4642ba33213147fc4cca0c
Augusto <lagrange.tesla@gmail.com>
2020-11-22 23:05:27 -0600

    Se agregaron más consejos

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -8,14 +8,5 @@ export default () => {
     divElement.innerHTML = views;
     const boton = divElement.querySelector('#btnClick');
     boton.addEventListener("click",  submitForm);
-        
-        // Fires the alert
-        // Swal.fire(
-        //     'Gracias',
-        //     'En breve llegará te llegará un correo con las indicaciones necesarias para el webinar!',
-        //     'success'
-        // )
-
-
     return divElement;
 }
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,24 +1,6 @@
 // imports
 import Swal from 'sweetalert2';
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-    apiKey: "AIzaSyAZnZVxU996ZExmhpv99nr9L_MuMND6bhM",
-    authDomain: "webinar-115f7.firebaseapp.com",
-    databaseURL: "https://webinar-115f7.firebaseio.com",
-    projectId: "webinar-115f7",
-    storageBucket: "webinar-115f7.appspot.com",
-    messagingSenderId: "725929204417",
-    appId: "1:725929204417:web:a354d34398622446c39240",
-    measurementId: "G-1GWGSKWETX",
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Hace referencia a los mensajes que van a la collecion de firebase
-let messageRef = firebase.database().ref('messages');
-
+import firebase from './db'
 
 // Envia los datos del formulario a firebase
 export function submitForm(e) {
@@ -28,7 +10,7 @@ export function submitForm(e) {
     const apellido = getInputValue("apellido");
     const correo = getInputValue("correo");
 
-    // saveMessage(nombre, apellido, correo);
+    
 
     //Fires the alert
     Swal.fire(
@@ -37,20 +19,21 @@ export function submitForm(e) {
         'success'
     );
 
-    console.log(nombre);
+    // let database = firebase.database();
+    // let refMessage = database.ref('usuarios');
+    let dataForm = {
+        name: nombre,
+        lastName: apellido,
+        email: correo
+    };
+    //ref.push(dataForm);
+    // console.log(database);
+    // console.log(refMessage);
+    console.log(dataForm);
+
+
 }
 
 function getInputValue(id) {
     return document.getElementById(id).value;
-}
-
-
-//Funcion para enviar los datos a firebase
-function saveMessage(nombre, apellido, correo) {
-    let newMessageRef = messageRef.push();
-    newMessageRef.set({
-        nombre = nombre,
-        apellido = apellido,
-        correo = correo
-    });
 }
